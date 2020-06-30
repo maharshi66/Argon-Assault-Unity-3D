372209b9e9debf06d8c8e6319888a7adcf5f17da
Maharshi Shah <maharshishah@mail.usf.edu>
2020-06-30 23:44:51 +0530

    Added Player Ship script and clamped X axis movement on user input

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 25f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffsetThisFrame;
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange); //restricts between limits
+        
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
