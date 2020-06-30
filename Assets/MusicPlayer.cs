06687c980a773e0775d241218882f70d010eb083
Maharshi Shah <maharshishah@mail.usf.edu>
2020-06-30 21:20:36 +0530

    Added Standard Asset and created circuit with WaypointCircuit script and Waypoint Progress Script for main camera

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+	// Start is called before the first frame update
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+	void Start()
+    {
+        Invoke("LoadFirstScene", 3f);
+    }
+
+    void LoadFirstScene()
+	{
+        SceneManager.LoadScene(1);
+	}
+    
+}
